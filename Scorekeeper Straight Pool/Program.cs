0ca1247aa75d4d7953d46472860a255b127f1774
David Sills <dsills6@wgu.edu>
2025-11-21 13:26:35 -0600

    Added a session wins counter, and more detail to the player win messages.

--- Scorekeeper Straight Pool/Program.cs
+++ Scorekeeper Straight Pool/Program.cs
@@ -13,13 +13,22 @@ Console.WriteLine("What score do you want to play to?");
 int maxScore = Convert.ToInt32(Console.ReadLine());
 int player1Score = 0;
 int player2Score = 0;
+int p1Max = 0;
+int p2Max = 0;
+int p2Wins = 0;
+int p1Wins = 0;
+
 PlayGame();
 void PlayGame()
 {   
+    Console.Clear();
     player1Score = 0;
     player2Score = 0;
+    if(p1Wins != 0 || p2Wins != 0)
+    {
+        DisplayWins();
+    }
     
-    Console.Clear();
     Console.WriteLine("Welcome to a new game! Who is going to break? Enter 1 or 2");
 
     int breaker = Convert.ToInt32(Console.ReadLine());
@@ -39,11 +48,22 @@ void Player1Turn()
     
     Console.WriteLine($"{player1}, It is your turn! Enter your score when you are finished. You currently have {player1Score} points.");
     int score = Convert.ToInt32(Console.ReadLine());
+    if(score > p1Max)
+    {
+        p1Max = score;
+    }
     player1Score += score;
     Console.Clear();
     if(player1Score >= maxScore)
     {
+        p1Wins++;
         Console.WriteLine($"Player 2 won with a score of {player1Score}");
+        Console.WriteLine();
+
+        Console.WriteLine($"{player2}'s highest score was {p2Max}");
+        Console.WriteLine($"{player1}'s highest score was {p1Max}");
+        
+        Console.WriteLine("------------------------------------------");
         Console.WriteLine("Do you want to play again? Y/N");
         string temp = Console.ReadLine()!.Trim().ToLower();
 
@@ -66,12 +86,22 @@ void Player2Turn()
     
     Console.WriteLine($"{player2}, It is your turn! Enter your score when you are finished. You currently have {player2Score} points.");
     int score = Convert.ToInt32(Console.ReadLine());
+    
+    if(score > p2Max)
+    {
+        p2Max = score;
+    }
     player2Score += score;
     Console.Clear();
 
     if(player2Score >= maxScore)
     {
+        p2Wins++;
         Console.WriteLine($"{player2} won with a score of {player2Score}");
+        Console.WriteLine();
+        Console.WriteLine($"{player1}'s highest score was {p1Max}");
+        Console.WriteLine($"{player2}'s highest score was {p2Max}");
+        Console.WriteLine("------------------------------------------");
         Console.WriteLine("Do you want to play again? Y/N");
         string temp = Console.ReadLine()!.Trim().ToLower();
 
@@ -87,4 +117,10 @@ void Player2Turn()
     Console.WriteLine($"{player2} scored {score}, and has a total score of {player2Score}");
     Console.WriteLine();
     Player1Turn();
+}
+
+void DisplayWins()
+{
+    Console.WriteLine($"{player1} has {p1Wins} wins.");
+    Console.WriteLine($"{player2} has {p2Wins} wins.");
 }
